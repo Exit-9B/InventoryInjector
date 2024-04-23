d35a3eb0f86719355f430d6426aecd6585d1d3c1
Parapets <southwest85b@gmail.com>
2024-04-23 17:41:16 +0100

    Resolve ambiguous constructors

--- src/Data/Config/CustomData.h
+++ src/Data/Config/CustomData.h
@@ -8,8 +8,9 @@ namespace Data
 		using Base = std::variant<std::nullptr_t, bool, double, std::string, std::wstring>;
 		using Base::Base;
 
-		template <typename Num> requires std::is_arithmetic_v<Num>
-		CustomData(Num a_value) : Base(static_cast<double>(a_value)) {}
+		CustomData(std::integral auto a_value) : Base(static_cast<double>(a_value)) {}
+		CustomData(float a_value) : Base(static_cast<double>(a_value)) {}
+		CustomData(bool a_value) : Base(a_value) {}
 
 		operator RE::GFxValue() const;
 
